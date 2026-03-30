1c76e4ee8c01e39485e1e95e0aa8245bcdfaca9a
Viktoriya Kibyeryeva <v.kibereva@gmail.com>
2026-03-30 11:33:44 +0300

    implemented block-matching

---
+++ block_matching.py
@@ -0,0 +1,45 @@
+import numpy as np
+import numpy.lib.stride_tricks as npw
+from typing import List
+from joblib import Parallel, delayed
+
+
+def block_matching(matrix: np.ndarray[tuple[int, int], np.dtype[np.int32]], 
+                   threshold: int, block_size: int = 5, window_size: int = 15):
+
+    blocks = npw.sliding_window_view(matrix, window_shape=(block_size, block_size))
+    pad_value = (window_size - block_size) // 2
+    windows = npw.sliding_window_view(np.pad(matrix, pad_width = pad_value, mode="reflect"), window_shape=(window_size, window_size)) # Make sure "reflect" is the best mode
+
+    result = np.empty(blocks.shape[:2], dtype=object)
+
+    results = Parallel(n_jobs=-1)(
+            delayed(process_column)(j , blocks, windows, threshold, block_size)
+            for j in range(blocks.shape[1])
+    )
+
+    for j, column in enumerate(results):
+        for i, block in enumerate(column):
+            result[i, j] = block
+    return result
+
+def process_column(column_idx: int, blocks, windows, threshold, block_size):
+    column_results = []
+    for i in range(blocks.shape[0]):
+        column_results.append(match_block(blocks[i, column_idx], windows[i, column_idx], threshold, block_size))
+    return column_results
+
+
+def match_block(target: np.ndarray[tuple[int, int], np.dtype[np.int32]], 
+                window: np.ndarray[tuple[int, int], np.dtype[np.int32]], 
+                threshold: int, block_size: int = 5) -> np.ndarray[tuple[int, int], np.dtype[np.int32]]:
+    candidates = npw.sliding_window_view(window, window_shape=(block_size, block_size))
+    diff = candidates - target
+    dist = np.sum(np.square(diff), axis = (-2, -1))
+    mask = dist < threshold ** 2
+    matched_blocks = candidates[mask]
+    if matched_blocks.shape[0] == 0:
+        return target.reshape(-1, 1, order = "F")
+    return matched_blocks.reshape(matched_blocks.shape[0], -1).T
+
+
---
+++ main.py
@@ -0,0 +1,20 @@
+import numpy as np
+from PIL import Image
+from block_matching import block_matching
+import time
+
+PATH_TO_FILE = 'some_lung.png'
+def main():
+    image = Image.open(PATH_TO_FILE)
+    image_matrix = np.array(image, dtype = np.int32, order = "F") # probably need to store in column major
+    now = time.time()
+    res = block_matching(image_matrix, 15)
+    for i in res:
+        print(i)
+        print()
+    print (time.time() - now)
+    
+if __name__ == "__main__":
+    main()
+
+
