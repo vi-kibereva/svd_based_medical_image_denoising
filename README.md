727f48b545c13d1ed833be3eed47ea9009163c91
Viktoriya Kibyeryeva <v.kibereva@gmail.com>
2026-04-07 20:28:37 +0300

    Added github links and updated project structure

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # svd_based_medical_image_denoising
 
 Links to videos:
-- Viktoria Kibyeryeva:
-- Yuliana Vus: https://youtu.be/KlS1YpdM-I0?si=is9gCq95d0rteSXZ
-- Olena Tumak: https://youtu.be/Dp_kqlenaB4?si=2fvTSO-89i7fcw9f
+- [Viktoria Kibyeryeva](https://youtu.be/vTENsqns6AA?si=j-Xrwz_9BxQmkH6f)
+- [Yuliana Vus](https://youtu.be/KlS1YpdM-I0?si=is9gCq95d0rteSXZ)
+- [Olena Tumak](https://youtu.be/Dp_kqlenaB4?si=2fvTSO-89i7fcw9f)
--- scripts/main.py
+++ scripts/main.py
@@ -5,15 +5,14 @@ from denoising import denoising
 from puttogether import aggregate_patches
 from noising import add_gaussian_noise
 import time
-#import pywt
+import pywt
 #import cv2
 import matplotlib.pyplot as plt
 
 
-PATH_TO_FILE = 'Some_lung.png'
-BLOCK_SIZE = 8
+PATH_TO_FILE = '../assets/Some_lung.png'
+BLOCK_SIZE = 15
 WINDOW_SIZE = 31
-SVD_THRESHOLD = 0.99
 DISTANCE_THRESHOLD = 10
 
 
@@ -25,7 +24,6 @@ def estimate_noise_sigma(image_matrix):
     
     return mad / 0.6745
 
-PATH_TO_FILE = 'COVID-1017.png'
 def main():
     image = Image.open(PATH_TO_FILE)
     image_matrix = np.array(image, dtype = np.int32, order = "F") # probably need to store in column major
@@ -42,7 +40,7 @@ def main():
     # image_matrix_noisy = add_poisson_noise(image_matrix)
     image_matrix_noisy = add_gaussian_noise(image_matrix, 10)
 
-    Image.fromarray(image_matrix_noisy).save("noisy_test_fragment.png")
+    Image.fromarray(image_matrix_noisy).save("../assets/noisy_test_fragment.png")
 
     estimated_sigma = estimate_noise_sigma(image_matrix_noisy)
     print(f"Estimated sigma: {estimated_sigma:.2f}")
@@ -59,14 +57,14 @@ def main():
     svd_start = time.time()
 
 
-    res_clean = denoising(res, SVD_THRESHOLD)
+    res_clean = denoising(res, estimated_sigma)
 
     print(f"SVD Denoising took: {time.time() - svd_start:.2f}s")
 
     clean_image = aggregate_patches(res_clean, image_matrix.shape, BLOCK_SIZE)
 
     clean_image = np.clip(clean_image, 0, 255).astype(np.uint8)
-    Image.fromarray(clean_image).save("denoised_result.png")
+    Image.fromarray(clean_image).save("../assets/denoised_result.png")
     print("Saved to denoised_result.png")
 
     print(f"Total time: {time.time() - now:.2f}s")
